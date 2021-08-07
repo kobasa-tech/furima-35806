2c704768c8b11ff62d2627b2188d4b7d29c1e85e
kobasa-tech <warped.trump@gmail.com>
2021-08-07 21:31:22 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_fee
+  belongs_to :province
+  belongs_to :delivery_days
 end
