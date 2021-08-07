7f1a835f2fb73a6e39387c7eafc691cf83770035
kobasa-tech <warped.trump@gmail.com>
2021-08-07 22:40:50 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  # バリデーションの設定
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category
+    validates :item_status
+    validates :delivery_fee
+    validates :province
+    validates :delivery_days
+  end
+
+  validates :price, presence: true,
+  format: { with: /\A[0-9]+\z/ },
+  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  # アソシエーションの設定
   has_one_attached :image
   belongs_to :user
 
