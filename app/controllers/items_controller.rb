02acb001ebf0c71b3131b7ec88a557c0133ad33e
kobasa-tech <warped.trump@gmail.com>
2021-08-08 16:08:11 +0900

    createアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image, :name, :description, :category_id, :item_status_id, :delivery_fee_id, :province_id, :delivery_days_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
