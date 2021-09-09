593450f10c0e304cb6f884aff85e1d4bdb5fc2b3
kobasa-tech <warped.trump@gmail.com>
2021-09-09 23:09:18 +0900

    jsファイルを複数枚投稿できるように記述

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -4,22 +4,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //生成した画像を表示する関数
     const createImageHTML = (blob) => {
-      //画像を表示するための要素を生成
+      //画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length;
+
+      // 表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
 
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input');
+      inputHTML.setAttribute('id', `item-image-${ imageElementNum }`);
+      inputHTML.setAttribute('name', 'item[images][]');
+      inputHTML.setAttribute('type', 'file');
+
       //生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
+      imageElement.appendChild(inputHTML);
       ImageList.appendChild(imageElement);
-    }
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob);
+      });
+    };
 
     //画像を取得してURLを生成
     document.getElementById('item-image').addEventListener('change', (e) => {
-      const imageContent = document.querySelector('.click-upload img');
-      if (imageContent){
-        imageContent.remove();
-      }
+
+      // 1枚投稿時のプレビュー枚数制限
+      // const imageContent = document.querySelector('.click-upload img');
+      // if (imageContent){
+      //   imageContent.remove();
+      // }
+      // 1枚投稿時のプレビュー枚数制限
+
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
